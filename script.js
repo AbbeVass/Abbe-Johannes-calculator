cc0a887a7546c5a3c4d0a4abc097defc52fc5483
AbbeVass <134689455+AbbeVass@users.noreply.github.com>
2023-08-30 16:41:17 +0200

    Added semicolon to script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function AddDecimal() {
 
     if (isDecimalAllowed()) {
         ClearError();
-        document.getElementById("display").textContent += "."
+        document.getElementById("display").textContent += ".";
     }
 }
 
